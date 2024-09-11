fe538ff8bb18e4c808950851fb1f712159705b53
gapragapr <gapragapr3@mail.ru>
2024-09-11 17:18:13 +0500

    add base routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { store } from './store/store.ts'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
