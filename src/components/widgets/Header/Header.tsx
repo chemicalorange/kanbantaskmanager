6e807aa2631d319192c9c7129e5add4c24af2d81
gapragapr <gapragapr3@mail.ru>
2024-09-30 10:48:39 +0500

    complete header

--- src/components/widgets/Header/Header.tsx
+++ src/components/widgets/Header/Header.tsx
@@ -1,10 +1,14 @@
 import { useAppSelector } from '../../../store/hooks'
 import styles from './Header.module.css'
 import Button from '../../shared/Button/Button'
+import CreateTaskModal from '../../../modals/CreateTaskModal/CreateTaskModal'
+import useModal from '../../../hooks/useModal'
 
 const Header = () => {
   const currentBoardId = useAppSelector(state => state.appSlice.currentBoardId)
   const currentBoard = useAppSelector(state => state.appSlice.boards.filter(item => item.id === currentBoardId)[0])
+
+  const [isShowing, toggle] = useModal()
 
   return (
     <header className={styles.header}>
@@ -12,8 +16,9 @@ const Header = () => {
             <p>{currentBoard.name}</p>
         </div>
         <div>
-            <Button title='+ Add New Task' color='purple' onClickHandler={() => {}} />
+            <Button title='+ Add New Task' color='purple' onClickHandler={toggle} />
         </div>
+        {isShowing && <CreateTaskModal closeModal={toggle} /> }
     </header>
   )
 }
