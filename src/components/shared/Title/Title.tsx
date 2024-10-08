17a18830522210b855c733d7c3d4858942e2de74
gapragapr <gapragapr3@mail.ru>
2024-10-08 17:55:47 +0500

    fix css

--- src/components/shared/Title/Title.tsx
+++ src/components/shared/Title/Title.tsx
@@ -1,3 +1,5 @@
+import styles from './Title.module.css'
+
 type TitleProps = {
     title: string,
 }
@@ -5,7 +7,7 @@ type TitleProps = {
 const Title = ({title}: TitleProps) => {
   return (
     <div>
-        <h2>{title}</h2>
+        <h2 className={styles.text} >{title}</h2>
     </div>
   )
 }
